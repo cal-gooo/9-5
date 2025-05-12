d051ccfd1ed4c5ac8cead7270a221a64cf8f3d25
Barry <53959224+cal-gooo@users.noreply.github.com>
2025-05-12 19:54:47 -0400

    added blossom

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,57 +1,63 @@
-import { useNDKSessionLogout } from "@nostr-dev-kit/ndk-hooks";
-import { useNavigate } from "react-router-dom";
+import { useNDKSessionLogout } from '@nostr-dev-kit/ndk-hooks';
+import { useNavigate } from 'react-router-dom';
 
 export default function Profile() {
-  const logout = useNDKSessionLogout();
-  const navigate = useNavigate();
+	const logout = useNDKSessionLogout();
+	const navigate = useNavigate();
 
-  const handleLogout = () => {
-    logout();
-    localStorage.clear();
-    console.log("User logged out successfully");
-  };
+	const handleLogout = () => {
+		logout();
+		localStorage.clear();
+		console.log('User logged out successfully');
+	};
 
-  const settings = [
-    { id: 1, name: "Edit Profile", action: () => console.log("Edit Profile") },
-    {
-      id: 2,
-      name: "Keys",
-      action: async () => {
-        navigate("/profile/keys");
-      },
-    },
-    {
-      id: 3,
-      name: "Privacy Settings",
-      action: () => console.log("Privacy Settings"),
-    },
-    { id: 4, name: "Logout", action: () => handleLogout() },
-  ];
+	const settings = [
+		{
+			id: 1,
+			name: 'Edit Profile',
+			action: () => {
+				navigate('/profile/edit');
+			},
+		},
+		{
+			id: 2,
+			name: 'Keys',
+			action: async () => {
+				navigate('/profile/keys');
+			},
+		},
+		{
+			id: 3,
+			name: 'Privacy Settings',
+			action: () => console.log('Privacy Settings'),
+		},
+		{ id: 4, name: 'Logout', action: () => handleLogout() },
+	];
 
-  return (
-    <div className="bg-sky-800 p-4">
-      {/* Profile Picture */}
-      <div className="flex flex-col items-center mb-6">
-        <img
-          src="https://t3.ftcdn.net/jpg/07/24/59/76/360_F_724597608_pmo5BsVumFcFyHJKlASG2Y2KpkkfiYUU.jpg"
-          alt="Profile"
-          className="w-32 h-32 rounded-full shadow-md"
-        />
-        <h1 className="text-xl font-bold mt-4 text-gray-800">John Doe</h1>
-      </div>
+	return (
+		<div className='bg-sky-800 p-4'>
+			{/* Profile Picture */}
+			<div className='flex flex-col items-center mb-6'>
+				<img
+					src='https://t3.ftcdn.net/jpg/07/24/59/76/360_F_724597608_pmo5BsVumFcFyHJKlASG2Y2KpkkfiYUU.jpg'
+					alt='Profile'
+					className='w-32 h-32 rounded-full shadow-md'
+				/>
+				<h1 className='text-xl font-bold mt-4 text-gray-800'>John Doe</h1>
+			</div>
 
-      {/* Settings List */}
-      <ul className="space-y-4">
-        {settings.map((setting) => (
-          <li
-            key={setting.id}
-            onClick={setting.action}
-            className="bg-white p-4 rounded-md shadow hover:bg-gray-50 cursor-pointer transition"
-          >
-            <p className="text-gray-800 font-medium">{setting.name}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+			{/* Settings List */}
+			<ul className='space-y-4'>
+				{settings.map((setting) => (
+					<li
+						key={setting.id}
+						onClick={setting.action}
+						className='bg-white p-4 rounded-md shadow hover:bg-gray-50 cursor-pointer transition'
+					>
+						<p className='text-gray-800 font-medium'>{setting.name}</p>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import tailwindcss from '@tailwindcss/vite';
 import vitePluginSvgr from 'vite-plugin-svgr';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [preact(), tailwindcss(), vitePluginSvgr()],
+	plugins: [preact(), tailwindcss(), vitePluginSvgr(), nodePolyfills()],
 });
